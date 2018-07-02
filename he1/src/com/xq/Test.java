5c899098574b5115152371cedfdae569237c535e
xiaoqiang <467924036@qq.com>
2018-07-03 00:10:45 +0800

    初始化

---
+++ he1/src/com/xq/Test.java
@@ -0,0 +1,14 @@
+package com.xq;
+
+/**
+ * Created by xiaoqiang on 2018/7/3.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("a1");
+        System.out.println("a2");
+        System.out.println("a3");
+        System.out.println("a4");
+        System.out.println("a5");
+    }
+}
